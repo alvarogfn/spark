2003cd282969150fd471d04c17ccb3d0e586fc35
alvaro.neto <62889807+alvarogfn@users.noreply.github.com>
2024-12-27 10:46:36 -0300

    feat(app): add shadcn

---
+++ packages/app/relay.config.js
@@ -0,0 +1,6 @@
+export default {
+  excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+  language: "typescript",
+  schema: "../server/src/schema.graphql",
+  src: "./src",
+};
